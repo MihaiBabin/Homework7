33ba9204b6f8043ab7f2fd80c5488582db01825f
MihaiBabin <Mihaibabin@gmail.com>
2025-05-15 23:25:05 +0300

    Add project files.

---
+++ Homework7/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework7
+{
+    using System;
+    #region Note Student
+    //struct Student
+    //{
+    //    public string Nume;
+    //    public double[] Note;
+    //}
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Student stud;
+    //        Console.WriteLine("Introduceti numele studentului");
+    //        stud.Nume = Console.ReadLine();
+    //        Console.WriteLine("Introduceti notele studentulul separate prin spatiu");
+    //        var text = Console.ReadLine();
+    //        double[] Note = Array.ConvertAll(text.Split(" "), double.Parse);
+    //        Console.WriteLine($"Notele studentului {stud.Nume} sunt:");
+    //        foreach (int notele in Note)
+    //        {
+    //            Console.WriteLine(notele);   //afisare toate notele
+    //        }
+    //        double med = Note.Sum() / Note.Length;
+    //        Console.WriteLine($"Media studentului este: {med}");
+    //    }
+
+    //}
+    #endregion
+
+    #region MinMax 
+    //class MinMax
+    //{
+    //    static void Main() 
+    //    {
+    //        Console.WriteLine("Introduceti numerele");
+    //        var text = Console.ReadLine();
+    //        double[] numere = Array.ConvertAll(text.Split(" "), double.Parse);
+    //        Array.Sort(numere);
+    //        int max = numere.Length-1;
+    //        Console.WriteLine($"Minimul este: {numere[0]}");
+    //        Console.WriteLine($"Maximul este: {numere[max]}");
+    //    }
+    //}
+    #endregion
+
+  
+}
